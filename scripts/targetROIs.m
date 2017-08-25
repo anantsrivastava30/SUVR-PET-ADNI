b733f732ca52357cc0c380298793fac2bcca0b51
anant <anantsrivastava30@gmail.com>
2017-08-24 17:40:49 -0700

    included the parcellation

---
+++ scripts/targetROIs.m
@@ -0,0 +1,32 @@
+clear;
+marsbar('on');
+
+% Basing the root to this directory
+xDoc = xmlread('./AAL.xml');
+
+labels = xDoc.getElementsByTagName('label');
+
+for k = 0:labels.getLength-1
+	TargetRegion = labels.item(k);
+
+	name = TargetRegion.getElementsByTagName('name'); 
+	val = TargetRegion.getElementsByTagName('index');
+
+	name = name.item(0).getFirstChild.getData;
+	val = val.item(0).getFirstChild.getData;
+
+	% {.} ensures space between boolean expression
+	prefix = '../1_Target/T'; 
+
+	func = strcat({'img == '}, {char(val)});
+	OutName = strcat(prefix, char(name), '.nii');
+	sprintf('%s \n',OutName)
+
+	BuildRoi = maroi_image(struct('vol', spm_vol('wAAL.nii'), ...
+    						'binarize',0,'func',func));
+
+	D = maroi_matrix( BuildRoi );
+	save_as_image(D, OutName);
+
+end
+
